79c44c14ad66e4f993fc4c056b0ebbebc200c191
bestideasblogger <135578437+thedevabror@users.noreply.github.com>
2025-06-26 16:04:27 +0500

    First commit

--- src/Left.tsx
+++ src/Left.tsx
@@ -6,10 +6,10 @@ function Left() {
         <div className='header' style={{width:"100%",height:"10%",display:"flex", justifyContent:"space-between"}}>
             <img src={logo} alt="Logo" style={{height:"80%",width:"auto"}} />
             <div style={{height:"80%", minWidth:"20%", marginTop:"4%"}}>
-                <span style={{marginLeft:'7%'}}>
+                <a style={{marginLeft:'7%'}} href='https://www.instagram.com/reel/DKhRThYIsaU/?igsh=emJ0NGljdzFuZ25y'>
 
                     <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="-2 -2 24 24"><g fill="#fff"><path d="M14.017 0h-8.07A5.954 5.954 0 0 0 0 5.948v8.07a5.954 5.954 0 0 0 5.948 5.947h8.07a5.954 5.954 0 0 0 5.947-5.948v-8.07A5.954 5.954 0 0 0 14.017 0zm3.94 14.017a3.94 3.94 0 0 1-3.94 3.94h-8.07a3.94 3.94 0 0 1-3.939-3.94v-8.07a3.94 3.94 0 0 1 3.94-3.939h8.07a3.94 3.94 0 0 1 3.939 3.94v8.07z"/><path d="M9.982 4.819A5.17 5.17 0 0 0 4.82 9.982a5.17 5.17 0 0 0 5.163 5.164a5.17 5.17 0 0 0 5.164-5.164A5.17 5.17 0 0 0 9.982 4.82zm0 8.319a3.155 3.155 0 1 1 0-6.31a3.155 3.155 0 0 1 0 6.31z"/><circle cx="15.156" cy="4.858" r="1.237"/></g></svg>
-                </span>
+                </a>
                 <span style={{marginLeft:'7%'}}>
 
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 16 16"><path fill="#fff" d="M8 0a8 8 0 0 1 1.083 15.927v-5.755h2.07l.326-2.113H9.083V6.903c0-.829.255-1.57.971-1.65l.132-.007h1.313V3.401l-.291-.036c-.28-.03-.711-.064-1.35-.064c-1.86 0-2.978.956-3.05 3.123l-.004.228V8.06H4.825v2.113h1.98v5.74A8.002 8.002 0 0 1 8 0"/></svg>
